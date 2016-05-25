b9da2fe803d81b3333fdf6d93a912a566218cdb0
Pashok <pu.lysak@gmail.com>
2016-05-25 16:51:42 +0300

    add maps

--- app/src/main/java/ru/sudoteam/cyclecomputer/fragments/RouteFragment.java
+++ app/src/main/java/ru/sudoteam/cyclecomputer/fragments/RouteFragment.java
@@ -2,7 +2,27 @@ package ru.sudoteam.cyclecomputer.fragments;
 
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.sudoteam.cyclecomputer.R;
+import ru.sudoteam.cyclecomputer.activities.MapActivity;
 
 public class RouteFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_route, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        startActivity(new Intent(getActivity(), MapActivity.class));
+        super.onStart();
+    }
 }
